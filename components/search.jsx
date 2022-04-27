89a57eab7ef0c8c4d2422abecc8552ca696604c3
Alejandra Espinoza <espinozalezama@gmail.com>
2022-04-27 10:17:38 -0500

    Redux installed, search input logic

--- components/search.jsx
+++ components/search.jsx
@@ -1,10 +1,23 @@
-import styles from './search.module.css'
+import { useState } from "react";
+import { useEffect } from "react";
+import styles from "./search.module.css";
+
 function SearchBar() {
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    console.log(input);
+  }, [input]);
+
   return (
     <div className={styles.main}>
-    <input className={styles.input} placeholder="Search for any job, title, keywords or company"></input>
+      <input
+        className={styles.input}
+        placeholder="Search for any job, title, keywords or company"
+        onChange={(e) => setInput(e.target.value.toLowerCase())}
+      ></input>
     </div>
-  )
+  );
 }
 
-export default SearchBar
+export default SearchBar;
